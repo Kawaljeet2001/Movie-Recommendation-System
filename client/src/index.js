4436fcb134df7196ca5c2d542466dc45b675d195
Kawaljeet Singh Batra <batrakawaljeetsingh@gmail.com>
2021-03-07 10:30:40 +0530

    added the new navbar and footer containers

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {BrowserRouter, Switch , Route} from 'react-router-dom';
 import App from './App';
-
+import Recommendations from "./Components/Recommendations";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Navbar/>
+    <Switch>
+      <Route exact path = "/" component = {App}/>
+      <Route path = "/recommend" component = {Recommendations}/>
+    </Switch>
+    <Footer />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
