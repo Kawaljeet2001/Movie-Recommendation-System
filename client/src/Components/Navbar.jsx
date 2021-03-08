e893c94387ec2901a849e66eda5ae76eaa2dceac
Kawaljeet Singh Batra <batrakawaljeetsingh@gmail.com>
2021-03-08 12:19:31 +0530

    changed the styling of spinner

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className = "Navbar">
-           <div className = "logo">
-                <h2>M R S</h2>
-           </div>
-           <div className = 'name'>
+        <div className="Navbar">
+            <div className="logo">
+                <Link className = "logo-link" to = "/">
+                    <h2>M R S</h2>
+                </Link>
+            </div>
+            <div className='name'>
                 <p>KSB</p>
-           </div>
+            </div>
         </div>
     )
 }
--- client/src/Components/Spinner.jsx
+++ client/src/Components/Spinner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Spinner = () => {
     return (
-        <div>
+        <div className = "spinner-div">
             <i className="fa fa-circle-o-notch fa-3x " aria-hidden="true"></i>
         </div>
     )
